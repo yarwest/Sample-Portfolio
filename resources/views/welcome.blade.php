e5ac545ca1b741c4cba51b0f127acbf2c821e4cf
Yarno Boelens <yarnoboelens@gmail.com>
2016-11-20 22:19:51 +0100

    Added Laravel and Bootstrap theme

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <!-- Styles -->
+        <link rel="stylesheet" type="text/css" href="https://bootswatch.com/darkly/bootstrap.min.css">
+        
+    </head>
+    <body>
+        <div class="jumbotron">
+            <div class="container">
+                <h1>Hello World</h1>
+            </div>
+        </div>
+    </body>
+</html>
