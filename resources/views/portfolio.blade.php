1b13b46538b78721e5c48ac7f45b95bb4b7a394c
Yarno Boelens <yarnoboelens@gmail.com>
2016-11-21 23:13:52 +0100

    Added images and small styling

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -16,18 +16,24 @@
     <body>
         <div class="jumbotron">
             <div class="container">
-                <h1>Portfolio</h1>
-                @foreach($portfolio_items as $item)
-                    <div class="panel panel-default">
-                        <div class="panel-heading">{{$item->title}}</div>
-                        <div class="panel-body">
-                            <img src="/img/{{$item->image}}.jpg" style="height:200px; width:200px; margin-left:calc(50% - 100px);">
-                        </div>
-                        <div class="panel-footer">
-                            <p>{{$item->description}}</p>
-                        </div>
+                <div class="row">
+                    <div class="col-md-2"></div>
+                    <div class="col-sm-8" style="margin: auto;">
+                        <h1>Portfolio</h1>
+                        @foreach($portfolio_items as $item)
+                            <div class="panel panel-default">
+                                <div class="panel-heading">{{$item->title}}</div>
+                                <div class="panel-body" style="padding: 0;">
+                                    <div style="height:230px; width:100%; background-image:url('/img/{{$item->image}}.jpg'); background-size:cover; background-position: center center;"></div>
+                                </div>
+                                <div class="panel-footer">
+                                    <p>{{$item->description}}</p>
+                                </div>
+                            </div>
+                        @endforeach
                     </div>
-                @endforeach
+                    <div class="col-md-2"></div>
+                </div>
             </div>
         </div>
     </body>
